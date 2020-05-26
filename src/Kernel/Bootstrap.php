e8a405c1ebe17709cf2d58354bf45499ace34424
SentryXSI <34962312+SentryXSI@users.noreply.github.com>
2020-05-26 23:58:38 +1000

    update bootstrap

--- src/Kernel/Bootstrap.php
+++ src/Kernel/Bootstrap.php
@@ -1,12 +1,16 @@
 <?php
 declare(strict_types=1)
 
-namespace App/Kernel;
+namespace App\Kernel;
 
 final class Bootstrap {
 
     public function __construct(){
+        $this->prepare();
+    }
+
+    public function prepare()
+    {
         echo 'Welcome to StormWriter v.0.0.1';
     }
 }
-
